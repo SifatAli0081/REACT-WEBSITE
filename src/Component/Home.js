0ccb7b689a01cba9ded1136f30522b9325d9187c
SifatAli0081 <sifatali616@gmail.com>
2022-07-09 07:23:58 +0600

    first commit

---
+++ src/Component/Home.js
@@ -0,0 +1,26 @@
+import React, {useState , useEffect} from "react";
+
+const Home = ()=> {
+    const [posts,setPosts] = useState ([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users?fbclid=IwAR2T27a8Y81n3VtgwsXg5KSfeeezGSFwOt1Trl-R9J_UAlMzjQrNGOlap7g')
+        .then((response) => response.json())
+        .then((data) =>{
+            console.log(data);
+            setPosts(data[10]);
+        })
+        .catch((error) => {
+            console.log(error);})
+    },[]);
+
+    return (
+        <div> 
+           {posts.titel}
+           <h1>Home</h1>
+
+        </div>
+    );
+};
+
+export default Home;
